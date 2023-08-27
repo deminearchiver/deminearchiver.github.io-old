c5b80e69a26cf63abdd088231af284a9c6442c01
DeMineArchiver <minearchiver@gmail.com>
2023-08-27 11:23:07 +0300

    Button

---
+++ src/lib/ui/components/button/Button.tsx
@@ -0,0 +1,12 @@
+import { Component, JSX, ParentComponent } from "solid-js";
+import styles from "./Button.module.css";
+
+const Button: ParentComponent = (props) => {
+  return (
+    <button class={styles["button"]}>
+      {props.children}
+    </button>
+  )
+}
+
+export default Button;
